aa764a32d24aefb94f6a17d29bf8ad3521fe9541
Yury Prokhorov <5071177@gmail.com>
2021-12-06 14:56:20 +0300

    Score added Reset alert added

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -19,14 +19,35 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
+    @State private var userScore = 0
+    
+    @State private var restartingGame = false
+    @State private var numberOfTries = 0
+    @State private var restartTitle = ""
+    
+    
+    func checkNumberOfTries () {
+        if numberOfTries == 8 {
+            restartingGame = true
+            restartTitle = "Last attempt, try again"
+        }
+        else {
+            return restartingGame = false
+        }
+    }
+    
     func flagTapped (_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else{
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong, this is flag of \(countries[number])"
+            userScore -= 1
         }
         
         showingScore = true
+        numberOfTries += 1
+        checkNumberOfTries()
     }
     
     
@@ -34,6 +55,19 @@ struct ContentView: View {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    
+    
+    func reset() {
+        showingScore = false
+        scoreTitle = ""
+        
+        userScore = 0
+        
+        restartingGame = false
+        numberOfTries = 0
+        restartTitle = ""
+    }
+    
     
     var body: some View {
         
@@ -68,11 +102,14 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(userScore)")
         }
-            Spacer()
+                        .alert(restartTitle, isPresented: $restartingGame) {
+                            Button("Try again", action: reset)
+        }
                 Spacer()
-                Text("Score: ???")
+                Spacer()
+                Text("Score: \(userScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 Spacer()
