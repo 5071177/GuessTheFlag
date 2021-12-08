a4a297b4e0b601dcb5710cb4ade3aa0e2cd41f98
Yury Prokhorov <5071177@gmail.com>
2021-12-08 13:52:19 +0300

    Added view composition for flag image

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,7 +7,16 @@
 
 import SwiftUI
 
-
+struct FlagImage: View {
+    var text: String
+    
+    var body: some View {
+        Image(text)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
+    }
+}
 
 
 
@@ -91,7 +100,7 @@ struct ContentView: View {
                         Button {
                             flagTapped(number)
                         } label: {
-                            Image(countries[number]).renderingMode(.original).clipShape(Capsule()).shadow(radius: 5)
+                            FlagImage(text: countries[number])
                         }
                     }
                     }
